d74bf17c4e0f6a0b22678590d12021dd52c80f4d
Gargi-123 <53333842+Gargi-123@users.noreply.github.com>
2020-12-01 17:00:24 +0530

    updated

--- user_serach_dashboard.js
+++ user_serach_dashboard.js
@@ -9,7 +9,7 @@ function serch_restaurant() {
             let firstletter = city[0].toUpperCase()
             city = city.slice(1, city.length)
             console.log(city)
-            fetch(`http://localhost:3000/data/?City=${firstletter + city}`).then(response => response.json()).then(data => renderDom(data))
+            fetch(`https://swiggy-clone-json-server.herokuapp.com/?City=${firstletter + city}`).then(response => response.json()).then(data => renderDom(data))
         }
     }
 }
